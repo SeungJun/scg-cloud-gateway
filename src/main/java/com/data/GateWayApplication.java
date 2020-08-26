ab54a37e64351e3f27e33b0abc8665f64b7175e1
원승준(Weon Seungjun)(SeungJun) <SeungJun@ncsoft.com>
2020-08-26 17:57:48 +0900

    proto init

--- src/main/java/com/data/GateWayApplication.java
+++ src/main/java/com/data/GateWayApplication.java
@@ -1,16 +1,19 @@
-package com.ncsoft.gamedata;
+package com.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class GateWayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GateWayApplication.class, args);
 
-		/*new SpringApplicationBuilder(com.ncsoft.gamedata.GateWayApplication.class)
-				  .properties("spring.config.name=application")
-				  .run(args);*/
+
+//		new SpringApplicationBuilder(GateWayApplication.class)
+//				  .properties("spring.config.name=application.yml")
+//				  .run(args);
 	}
 }
